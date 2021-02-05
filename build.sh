bce60facbdd97113d81d4ec101b8bcb1cefd2ca6
auooru <auooru@163.com>
2021-02-05 10:07:58 +0800

    update: v1.7

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --build-arg CN=1 --build-arg INTRANET=http://192.168.138.166/files -t swoole-server:1.5 .
+docker build --build-arg CN=1 --build-arg INTRANET=http://192.168.138.166/files -t swoole-server:1.7 .
